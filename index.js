81c6a9489a84424db77f9e2e05ff53f42245750a
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-08-22 16:45:32 +0100

    completed commit hook ressponse

--- index.js
+++ index.js
@@ -2,13 +2,36 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
+var _ = require('lodash')
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+
+  app.on('push', async context => {
+    let repository = {
+      id: null,
+      node_id: null,
+      name: null,
+      full_name: null,
+      private: null
+    }
+
+    let repo_result = _.pick(context.payload.repository, _.keys(repository));
+
+
+    let model = {
+      sender: null,
+      installation: null,
+      commits: null
+    };
+    let result = _.pick(context.payload, _.keys(model));
+    result.repository = repo_result
+    // console.log(result)
+    app.log(result);
+    //send to microservice
+
+
   })
 
   // For more information on building apps:
