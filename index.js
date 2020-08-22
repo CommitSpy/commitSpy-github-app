2a45bb3a162e7c460e7fc829a58648f30febe26b
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-08-22 17:40:16 +0100

    added hook

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-var _ = require('lodash')
+var _ = require('lodash');
+var axios = require('axios');
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
@@ -22,6 +23,6 @@ module.exports = app => {
     app.log(result);
     //send to microservice
 
-
+    axios.post('https://smee.io/yrt37hGR4UhaKry', result)
   })
 }
